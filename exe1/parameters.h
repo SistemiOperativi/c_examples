55e8a018a24f2d4a7ecb6d16ececaf31edc9f62d
RomoloMarotta <romolo.marotta@gmail.com>
2022-01-12 15:13:15 +0100

    Added first files

--- exe1/parameters.h
+++ exe1/parameters.h
@@ -5,11 +5,11 @@
 
 #define abort(msg) do{printf(msg);exit(1);}while(0)
 
-#define FILE_SIZE (32*1024*sizeof(short))
-#define CORES 8
-#define MEM_PER_CORE (256*sizeof(short))
-#define MAX_SHARED_MEM (8*1024)
-#define FILENAME "dataset.bin"
+#define FILE_SIZE (32*1024*sizeof(short))   // FILE SIZE = 64KB
+#define FILENAME "dataset.bin"			
+#define CORES 8								// CPU UNITS
+#define MEM_PER_CORE (256*sizeof(short))    // MEMORY ALLOCABLE FOR EACH INDEPENDENT THREAD PROCESS = 512B
+#define MAX_SHARED_MEM (8*1024)             // MAXIMUM ALLOCABLE SHARED MEMORY SEGMENT = 8KB 
 
 
 #endif
